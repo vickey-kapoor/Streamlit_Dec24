f58f850dbf0ad9c21c038a292c90733a70e44d59
vickey-kapoor <153152069+vickey-kapoor@users.noreply.github.com>
2024-12-13 12:04:13 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,10 +1,23 @@
 import streamlit as st
 import yfinance as yf
+import datetime
 
-st.title("Stock Analyzer.")
+st.title("Stock Analyzer")
 
-ticker_data = yf.Ticker("MSFT")
+stock_name = st.text_input("Enter the stock ticker: ", 'MSFT')
 
-ticker_df = ticker_data.history(period = '1d', start = '2020-01-01', end = '2023-12-31')
+start_date = st.date_input("Enter the start date: ", datetime.date(2020,1,1))
+end_date = st.date_input("Enter the end date: ", datetime.date(2024, 12, 13))
 
+ticker_data = yf.Ticker(stock_name)
+
+ticker_df = ticker_data.history(period = '1d', start = start_date, end = end_date)
+
+st.subheader("Stock Information")
 st.dataframe(ticker_df)
+
+st.subheader("Stock Closing Price")
+st.line_chart(ticker_df.Close)
+
+st.subheader("Stock Volume")
+st.line_chart(ticker_df.Volume)
